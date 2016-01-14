dfa08cfda0d5492449f413f7e02c6afac4c50416
Doug Cooper <douglas.cooper@qmul.ac.uk>
2016-01-14 11:52:45 +0000

    Replace  with  in version.php

--- version.php
+++ version.php
@@ -12,15 +12,14 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016011300;
+$plugin->version   = 2016011400;
 $plugin->requires  = 2013051408;
 $plugin->component = 'mod_pearson';
-$module->cron      = 0;
-$module->component = 'mod_pearson';
-$module->maturity  = MATURITY_STABLE;
-$module->release   = '1.0';
+$plugin->cron      = 0;
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0';
 
-$module->dependencies = array(
+$plugin->dependencies = array(
 		'mod_lti' => ANY_VERSION
 );
 
