ae7a04bf33c112335e805e4c414329207f169d9f
Doug Cooper <douglas.cooper@qmul.ac.uk>
2016-05-11 15:19:35 +0100

    Update to Moodle v3.0.3-compliant version

--- version.php
+++ version.php
@@ -12,14 +12,14 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015051200;
+$plugin->version   = 2015120800;
 $plugin->requires  = 2013051408;
-$module->cron      = 0;
-$module->component = 'mod_pearson';
-$module->maturity  = MATURITY_STABLE;
-$module->release   = '1.0';
+$plugin->cron      = 0;
+$plugin->component = 'mod_pearson';
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0';
 
-$module->dependencies = array(
+$plugin->dependencies = array(
 		'mod_lti' => ANY_VERSION
 );
 
